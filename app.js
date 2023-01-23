0adc9d31bf5e6d12bf91fbdcf3848414981e168e
pacemaximiliano <pace.maximiliano@gmail.com>
2023-01-23 20:40:59 -0300

    modificacion app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+
 //const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static('public'));
 
-app.listen(3005, function () {
+/*app.listen(3005, function () {
     console.log('Srvidor corriendo');
-})
+})*/
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
+
+app.listen(PORT,function(){
+    console.log('Servidor funcionando' + PORT)
+})
+
