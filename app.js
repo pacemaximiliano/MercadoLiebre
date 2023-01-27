fe45d69b0101729ec00bbb61898b1f2466b26895
pacemaximiliano <pace.maximiliano@gmail.com>
2023-01-27 09:25:34 -0300

    login/register + agregado formato en home

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+app.get('/register', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
+
 app.listen(PORT,function(){
     console.log('Servidor funcionando' + PORT)
 })
